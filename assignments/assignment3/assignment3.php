a5ef4da3f1a15fc9a9b84a1283a940f044c53c86
Peter Hansson <a16petha@student.his.se>
2021-04-02 11:11:20 +0200

    Changed arraynames and updated layout

--- assignments/assignment3/assignment3.php
+++ assignments/assignment3/assignment3.php
@@ -1,14 +1,23 @@
 <html>
 
+<head>
+    <style>
+        /* CSS börjar här */
+        button {
+            margin-left: 10px;
+        }
+    </style>
+</head>
+
 <body>
     <?php
 
-    // Hämtar arrayen med informationen om tillverkare samt land
+    // Hämtar arrayen med informationen om tillverkare samt land via webservice
     $url = "https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/manufacturer/";
     $jsontext = file_get_contents($url);
     $arr = json_decode($jsontext);
 
-    // Formulär som hämtar alla tillverkare från arrayen via webservice och skickar vidare det valda alternativet
+    // Formulär som visar en lista med tillverkare
     echo "<form method='post' action='formRespSelectSelf.php'>";
     echo "<select name='country'>";
     // Visar upp värdet för tillverkare men skickar vidare landet för tillverkaren
@@ -16,10 +25,11 @@
         echo "<option value='" . $country[1] . "' >" . $country[0] . "</option>";
     }
     echo "</select>";
-    echo "<button>Go!</button>";
+    echo "<button>Skicka</button>";
     echo "</form>";
 
     ?>
+
 </body>
 
 </html>
--- assignments/assignment3/formRespSelectSelf.php
+++ assignments/assignment3/formRespSelectSelf.php
@@ -30,35 +30,34 @@
     <table border='1'>
         <?php
 
-
         // Hämtar landet från formuläret samt sätter default till inget så ingen info syns om personen ej valt något
         if (isset($_POST['country'])) {
-            $incountry = $_POST['country'];
+            $country = $_POST['country'];
         } else {
-            $incountry = "";
+            $country = "";
         }
 
-        // Hämtar det valda landet som sparats i $incountry och sparar det i en ny array
-        $url = "https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/vehicles/?country=" . $incountry;
+        // Hämtar det valda landet som sparats i $country och sparar det i en ny array
+        $url = "https://wwwlab.iit.his.se/gush/XMLAPI/vehiclesservice/vehicles/?country=" . $country;
         $jsontext = file_get_contents($url);
         $arr = json_decode($jsontext);
 
         // Visar en div med valt land ovanför tabellen
-        echo "<div>$incountry</div>";
         echo '<tr><th>Country</th><th>Model</th><th>Configuration</th><th>Horsepower</th></tr>';
         // Hämtar värdena i arrayen och loopar ut informationen som land samt uppgifter om fordonen.
-        foreach ($arr as $man) {
-            foreach ($man as $model) {
+        foreach ($arr as $manufacturer) {
+            foreach ($manufacturer as $info) {
                 echo "<tr>";
-                echo "<td>" . $incountry . "</td>";
-                echo "<td>" . $model[0] . "</td>";
-                echo "<td>" . $model[1] . "</td>";
-                echo "<td>" . $model[2] . "</td>";
+                echo "<td>" . $country . "</td>";
+                echo "<td>" . $info[0] . "</td>";
+                echo "<td>" . $info[1] . "</td>";
+                echo "<td>" . $info[2] . "</td>";
                 echo "</tr>";
             }
         }
 
         ?>
+
     </table>
 </body>
 
